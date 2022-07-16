718776377375d05619704e88e83c82e9b37dc0d7
who-hoo <who.ho3ov@gmail.com>
2022-07-16 17:09:17 +0900

    ✨ 전역 예외 핸들러 추가

---
+++ src/main/java/com/musinsa/category/exception/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.musinsa.category.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+
+	private String message;
+}
---
+++ src/main/java/com/musinsa/category/exception/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.musinsa.category.exception;
+
+import java.util.NoSuchElementException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
