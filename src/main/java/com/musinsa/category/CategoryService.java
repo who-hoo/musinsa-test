0e46dbab0481d444f84ef39eadd649f14e362fe2
who-hoo <who.ho3ov@gmail.com>
2022-07-17 18:54:56 +0900

    ⚡️ 캐싱 적용하여 성능 개선

--- src/main/java/com/musinsa/category/CategoryService.java
+++ src/main/java/com/musinsa/category/CategoryService.java
@@ -10,15 +10,20 @@ import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@EnableCaching
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
 
 	private final CategoryRepository categoryRepository;
 
+	@Cacheable("category")
 	@Transactional(readOnly = true)
 	public CategoriesResponse searchAllRootCategories() {
 		List<CategoryResponse> categories = categoryRepository.findAllJoinFetch().stream()
@@ -28,6 +33,7 @@ public class CategoryService {
 		return CategoriesResponse.from(categories);
 	}
 
+	@Cacheable(value = "category")
 	@Transactional(readOnly = true)
 	public CategoryResponse searchCategory(Long id) {
 		return categoryRepository.findCategoryAndSubCategoriesByIdJoinFetch(id).stream()
@@ -37,6 +43,7 @@ public class CategoryService {
 			.orElseThrow(() -> new NoSuchElementException("존재하지 않는 [카테고리 ID]입니다."));
 	}
 
+	@CacheEvict(value = "category", allEntries = true)
 	@Transactional
 	public CategoryResponse save(AddCategoryRequest addCategoryRequest) {
 		Category newCategory = addCategoryRequest.toEntity();
@@ -48,6 +55,7 @@ public class CategoryService {
 		return CategoryResponse.from(categoryRepository.save(newCategory));
 	}
 
+	@CacheEvict(value = "category", allEntries = true)
 	@Transactional
 	public CategoryResponse update(Long id, UpdateCategoryRequest updateCategoryRequest) {
 		//TODO: category, parent, subCategories 조회 후에 이들의 subCategories를 조회하기 위한 N+1 개선
@@ -74,6 +82,7 @@ public class CategoryService {
 		return CategoryResponse.from(updatedCategory);
 	}
 
+	@CacheEvict(value = "category", allEntries = true)
 	@Transactional
 	public void delete(Long id) {
 		Category category = categoryRepository.findById(id)
